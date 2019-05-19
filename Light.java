790c4dc5a36c67aa6ab29f6158d641112336abb0
Bradley Dufour <dufour.bradley@gmail.com>
2019-05-19 14:12:48 -0500

    Add Color and Road

--- Light.java
+++ Light.java
@@ -1,5 +1,38 @@
+import java.util.LinkedList;
+
 //Represents traffic light controlling a set of roads
 public class Light
 {
+	public enum Color
+	{
+		RED, YELLOW, GREEN;
+	}
 
+	private Color lightColor;
+	private LinkedList<Road> roadsControlled;
+
+	public Light()
+	{
+		lightColor = Color.RED;
+	}
+
+	public addRoad(Road newRoad)
+	{
+		roadsControlled.add(newRoad);
+	}
+
+	public LinkedList<Road> getRoads()
+	{
+		return roadsControlled;
+	}
+
+	public changeColor(Color newColor)
+	{
+		lightColor = newColor;
+	}
+
+	public getColor()
+	{
+		return lightColor;
+	}
 }
