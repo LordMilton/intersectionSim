25b755087aea8bede0ebeb95bbba7abcefb56b48
Bradley Dufour <dufour.bradley@gmail.com>
2019-05-21 09:55:19 -0500

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,8 @@
+//Controls actions necessary each tick
+public class Controller
+{
+	//How much longer the first car on a green light should take
+	static final float FIRST_CAR_TICK_MULTIPLIER = 1.5;
+
+
+}
