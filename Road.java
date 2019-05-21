838366f9aa0b44244ffdfdaac32ec26917eaef8f
Bradley Dufour <dufour.bradley@gmail.com>
2019-05-21 09:46:59 -0500

    Add car handling

--- Road.java
+++ Road.java
@@ -1,11 +1,29 @@
-import java.util.*
+import java.util.LinkedList;
 
 //Represents a queue of Cars waiting at an intersection
 public class Road
 {
-	private Queue<Car> line;
+	//Used as a queue, but all Cars need to be updated each tick
+	private LinkedList<Car> line;
 	
 	public Road()
 	{
+		line = new LinkedList<>();
+	}
+
+	public void addOneTick()
+	{
+		for(Car car:line)
+			car.addOneTick();
+	}
+
+	public void addCar(Car car)
+	{
+		line.add(car);
+	}
+
+	public Car removeCar()
+	{
+		return line.removeLast();
 	}
 }
