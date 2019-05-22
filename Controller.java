aac04fd672d790020dbe60c89adfb43f2156eb2c
Bradley Dufour <dufour.bradley@gmail.com>
2019-05-21 19:20:50 -0500

    Add instance variables and tick handling

--- Controller.java
+++ Controller.java
@@ -1,8 +1,27 @@
+import java.util.*;
+
 //Controls actions necessary each tick
 public class Controller
 {
 	//How much longer the first car on a green light should take
 	static final float FIRST_CAR_TICK_MULTIPLIER = 1.5;
+	private int tickCounter;
+	private LinkedList<Road> roads;
+	private LinkedList<Light> lights;
 
+	public Controller(Collection<Road> roads, Collection<Light> lights)
+	{
+		tickCounter = 0;
+		this.roads = new LinkedList<>(roads);
+		this.lights = new LinkedList<>(lights);
+	}
 
+	public void advanceOneTick()
+	{
+		tickCounter++;
+		for(Light light:lights)
+		{
+			light.addOneTick();
+		}
+	}
 }
