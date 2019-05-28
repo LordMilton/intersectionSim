8a1e318749b6731723b8b93ddd884028ff82becc
Bradley Dufour <dufour.bradley@gmail.com>
2019-05-28 16:19:51 -0400

    Remove First car cross multiplier

--- Controller.java
+++ Controller.java
@@ -3,8 +3,6 @@ import java.util.*;
 //Controls actions necessary each tick
 public class Controller
 {
-	//How much longer the first car on a green light should take
-	static final float FIRST_CAR_TICK_MULTIPLIER = 1.5;
 	private int tickCounter;
 	private LinkedList<Road> roads;
 	private LinkedList<Light> lights;
