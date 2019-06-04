94899aac67f8d13a9f8414af7ad070507a304716
Bradley Dufour <dufour.bradley@gmail.com>
2019-06-03 21:13:43 -0400

    Add carsPerTick handling

--- Road.java
+++ Road.java
@@ -9,14 +9,23 @@ public class Road
 	private Light controllingLight;
 	//Used as a queue, but all Cars need to be updated each tick
 	private LinkedList<Car> line;
+	//How many cars should arrive in this road per tick, can be any decimal value above 0
+	private double carsPerTick;
+	//How close to another car arriving in this Road, based around carsPerTick
+	//Can be above 1 at any given time but will never end a tick as such
+	private double nextCarProgress;
 	//All cars which have left line aka "crossed" the intersection
 	private LinkedList<Car> crossed;
 	
-	public Road(String identifier, Light light)
+	public Road(String identifier, Light light, double carsPerTick)
 	{
 		this.identifier = identifier;
 		line = new LinkedList<>();
 		controllingLight = light;
+		if(carsPerTick <= 0)
+			throw new IllegalArgumentException();
+		this.carsPerTick = carsPerTick;
+		nextCarProgress = 0;
 	}
 
 	public void addOneTick()
@@ -26,7 +35,16 @@ public class Road
 			line.peek().setCanCross();
 		}
 
+		nextCarProgress += carsPerTick;
+		while(nextCarProgress >= 1)
+		{
+			nextCarProgress--;
+			addCar(new Car());
+		}
+
 		Iterator i = line.descendingIterator();
+		//This flag will be repeatedly set in this loop, but we only care about the last car
+		//we check (which is the first car in line)
 		boolean doneCrossing = false;
 		while(i.hasNext())
 		{
