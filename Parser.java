fa247b2cba39b9278d9e4985aff914d458bb6f98
Bradley Dufour <dufour.bradley@gmail.com>
2019-06-13 19:01:06 -0400

    Create and use parsing method

--- Parser.java
+++ Parser.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
 import java.io.File;
 
-public class Main
+public class Parser
 {
+	public static parseFile(Scanner infile, LinkedList<Road> roads, LinkedList<Light> lights)
+	{
+		
+	}
+
 	public static void main(String[] args)
 	{
 		/*
-		   if(args.length != 2)
+		   if(args.length != 3)
 		   {
-		   System.out.println("One argument expected: input file");
+		   System.out.println("Two arguments expected: input file, ticks to run simulation for");
 		   }
 		   */
 		//Filename will be obtained from command line args
@@ -17,6 +22,8 @@ public class Main
 		//This will be obtained from command line args
 		int ticksToRun = 2000;
 
-
+		LinkedList<Road> roads = new LinkedList<>();
+		LinkedList<Light> lights = new LinkedList<>();
+		parseFile(scan, roads, lights);
 	}
 }
