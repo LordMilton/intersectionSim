e4e42fe8787f84d50ac688aa23e01d01e0f15636
Bradley Dufour <dufour.bradley@gmail.com>
2019-07-14 11:55:30 -0400

    skeleton methods

--- Analysis.java
+++ Analysis.java
@@ -2,7 +2,31 @@ import java.util.LinkedList;
 
 public class Analysis
 {
+	LinkedList<Light> lights;
+	LinkedList<Road> roads;
+
 	public Analysis(LinkedList<Light> lights, LinkedList<Road> roads)
+	{
+		this.lights = lights;
+		this.roads = roads;
+	}
+
+	public int roadAvgWait(String roadId)
+	{
+
+	}
+
+	public int roadMaxWait(String roadId)
+	{
+
+	}
+
+	public int lightAvgWait(String lightId)
+	{
+
+	}
+
+	public int lightMaxWait(String lightId)
 	{
 
 	}
