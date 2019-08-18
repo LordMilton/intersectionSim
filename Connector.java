a9028ded819edfbd56fb91011f0eb92db25154c3
Bradley Dufour <dufour.bradley@gmail.com>
2019-08-17 20:04:50 -0400

    Add skeleton methods

--- Connector.java
+++ Connector.java
@@ -1,4 +1,28 @@
+import java.util.LinkedList;
+
 public class Connector
 {
+	private Road entrance;
+	private Road exit;
+	private int ticksToCommute;
+	private LinkedList<Car> line;
+	//Tracks how much longer the associated car in the line needs to get to the nextexit Road
+	private LinkedList<Integer> carTimers;
 
+	public Connector(Road entrance, Road exit, int commuteTime)
+	{
+		this.entrance = entrance;
+		this.exit = exit;
+		this.ticksToCommute = commuteTime;
+	}
+
+	public void addOneTick()
+	{
+
+	}
+
+	public void addCar(Car newCar)
+	{
+
+	}
 }
